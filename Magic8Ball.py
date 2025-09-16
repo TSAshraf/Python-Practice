7bd15bd2f7aa9cdc5d91fb4e8c75701931047eb8
TSAshraf <139711304+TSAshraf@users.noreply.github.com>
2025-09-16 03:14:19 +0100

    Update Magic8Ball.py

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,12 +1,18 @@
 import random 
 
+# Asks for name, if there is no name a default name is given
 name = input("What is your name?")
+if name == "":
+  name = "The Nameless One"
+
+# Asks for a question, if there is no name a default name is given
 question = input("What is your question?")
-# question = "Is there an afterlife?"
+if question == "":
+  question = "Is there an afterlife?"
+
+# Randomly picks an answer from a preselected range of answers
 answer = ""
-
-random_number = random.randint(1, 9)
-
+random_number = random.randint(1, 10)
 if random_number == 1:
   answer = "Yes - definitely"
 elif random_number == 2:
@@ -25,8 +31,12 @@ elif random_number == 8:
   answer = "Outlook not so good"
 elif random_number == 9:
   answer = "Very doubtful"
+elif random_number == 10:
+  answer = "Hopefully so"
 else:
   answer = "Error"
 
+# Prints response
 print(f"{name} asks: {question}")
 print(f"Magic 8-Ball's answer: {answer}")
+
